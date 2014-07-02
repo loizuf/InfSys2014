7ba80cf5ea5f844c072ad0f41227e529406e2191
F. R <3razor16@gmail.com>
2014-07-02 17:10:51 +0200

    update MapElement

--- src/context/MapElement.java
+++ src/context/MapElement.java
@@ -1,6 +1,35 @@
 package context;
 
 public class MapElement {
+	
+	private boolean occupied;
+	private int x, y;
+	private double probability, length;
+	
+	public MapElement (int x, int y, double length, double probability, boolean occupied) {
+		this.x = x;
+		this.y = y;
+		this.probability = probability;
+		this.occupied = occupied;
+		
+	}
+
+	public boolean isOccupied() {
+		return occupied;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public double getProbability() {
+		return probability;
+	}
+	
 	//TODO Map Element aufsetzen! 
 	/**(X/Y Position / besetzt oder nicht? / 
 	 * Wahrscheinlichkeit ob es besetzt ist /
